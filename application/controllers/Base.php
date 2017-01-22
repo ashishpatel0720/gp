e8bb8ed97ce50a602a5133bc2f00cdf8fc733bb5
ashish <ashishpatel0720@gmail.com>
2017-01-22 05:07:31 -0800

    contact complete, will implement flashdata() tomarrao

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -13,8 +13,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Base extends CI_Controller
 {
-    public $header_data=[];
-    public $footer_data=[];
+    public $header_data = [];
+    public $footer_data = [];
+
     public function __construct()
     {
         parent::__construct();
@@ -22,9 +23,9 @@ class Base extends CI_Controller
         $this->load->helper('form');
         $this->load->helper('emailcontent');
         $this->load->model('bookmodel');
-
+        $this->load->model('basemodel');
         $this->load->library('form_validation');
-        $this->categories= [];
+        $this->categories = [];
         // $this->output->cache(3);
         $books = $this->bookmodel->fetchCategories();
         foreach ($books as $key => $value) {
@@ -32,24 +33,26 @@ class Base extends CI_Controller
         }
         $this->header_data['categories'] = $this->categories;
     }
+
     public function index()
     {
         $this->header_data['page_title'] = "Grabpustak | Read and Publish";
         $this->header_data['meta_title'] = "Grabpustak | Read and Publish";
-        $this->header_data['description']="Grabpustak is the online repository for books. Which contains the large variety of children, college books and large dataset of the nobels.";
+        $this->header_data['description'] = "Grabpustak is the online repository for books. Which contains the large variety of children, college books and large dataset of the nobels.";
 
         $home_data['top_books'] = $this->bookmodel->getBooksByLimit(5);
-        $home_data['featured'] =  $this->bookmodel->getBooksByLimit(5, $start=10, $type_array = false);
+        $home_data['featured'] = $this->bookmodel->getBooksByLimit(5, $start = 10, $type_array = false);
 
         $this->load->view('site/header_home', $this->header_data);
         $this->load->view('site/home', $home_data);
         $this->load->view('site/footer', $this->footer_data);
     }
+
     public function about()
     {
         $this->header_data['page_title'] = "About Us";
         $this->header_data['meta_title'] = "About Us";
-        $this->header_data['description']="Grabpustak is the online repository for books. Which contains the large variety of children, college books and large dataset of the nobels.";
+        $this->header_data['description'] = "Grabpustak is the online repository for books. Which contains the large variety of children, college books and large dataset of the nobels.";
         $this->footer_data['facebook'] = true;
         $this->footer_data['map'] = true;
 
@@ -58,16 +61,76 @@ class Base extends CI_Controller
         $this->load->view('site/about');
         $this->load->view('site/footer', $this->footer_data);
     }
-    public function contact()
+
+    public function contact($message = "no_message")
     {
         $this->header_data['page_title'] = "Contact Us";
         $this->header_data['meta_title'] = "Contact Us";
-        $this->header_data['description']="Contact us for any query or doubt.";
+        $this->header_data['description'] = "Contact us for any query or doubt.";
         $this->footer_data['facebook'] = true;
         $this->footer_data['map'] = true;
 
+
+        if (!empty($_POST)) {
+            $this->form_validation->set_rules('name', 'Name', 'trim|required|max_length[50]|alpha_numeric_spaces',
+                [
+                'required' => '%s is required.',
+                'max_length' => '%s must be of maximum 50 characters.',
+                'alpha' => '%s must contain only alphabet characters and spaces'
+            ]);
+            $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email', [
+                'required' => '%s is required.'
+            ]);
+            $this->form_validation->set_rules('subject', 'Subject', 'trim|required|max_length[100]', [
+                'max_length' => '%s must be of maximum 100 characters.',
+                'required' => '%s is required.'
+
+            ]);
+            $this->form_validation->set_rules('message', 'Message', 'trim|required|max_length[1000]', [
+                'required' => '%s is required.',
+                'max_length' => '%s must be of maximum 1000 characters.'
+            ]);
+            $this->form_validation->set_rules('mobile', 'Mobile Number', 'trim|required|exact_length[10]|numeric', [
+                'required' => '%s is required.',
+                'exact_length' => '%s must be of 10 digits.',
+                'numeric'=>'%s must contain numbers only.',
+            ]);
+            $this->form_validation->set_rules('place', 'Place', 'trim|required|max_length[200]', [
+                'required' => '%s is required.',
+                'max_length' => '%s must be of maximum 200 characters.',
+            ]);
+
+            if ($this->form_validation->run() == true) {
+                $name = $this->input->post('name');
+                $email = $this->input->post('email');
+                $subject = $this->input->post('subject');
+                $message = $this->input->post('message');
+                $mobile = $this->input->post('mobile');
+                $place = $this->input->post('place');
+
+                $contact_data = [
+                    'name' => $name,
+                    'email' => $email,
+                    'subject' => $subject,
+                    'message' => $message,
+                    'mobile'=>$mobile,
+                    'place'=>$place
+                   ];
+
+                $result = $this->basemodel->saveContactMessage($contact_data);
+                if ($result)
+                    $data['message'] = 'success';
+                else
+                    $data['message'] = 'sql_error';
+            }
+            else
+                $data['message']='validation_error';
+        }
+        else
+            $data['message'] = $message;
         $this->load->view('site/header', $this->header_data);
-        $this->load->view('site/contact');
+        $this->load->view('site/contact', $data);
         $this->load->view('site/footer', $this->footer_data);
+
     }
 }
--- application/views/site/contact.php
+++ application/views/site/contact.php
@@ -1,80 +1,101 @@
 
-		<!-- MAIN -->
-		<main class="site-main">
+<!-- MAIN -->
+<main class="site-main">
 
-            <!-- breadcrumb -->
-            <div class="container breadcrumb-page">
-                <ol class="breadcrumb">
-                    <li><a href="#">Home </a></li>
-                    <li class="active">Electronics</li>
-                </ol>
-            </div> <!-- breadcrumb -->
+    <!-- breadcrumb -->
+    <div class="container breadcrumb-page">
+        <ol class="breadcrumb">
+            <li><a href="/">Home </a></li>
+            <li class="active">Contact</li>
+        </ol>
+    </div> <!-- breadcrumb -->
 
-            <div class="container">
-                <div class="block-googlemap">
-                    
-                   <img src="/static/site/images/core/manit.jpeg" alt="Bhopal">
-                </div>
+    <div class="container">
+        <div class="block-googlemap">
 
-                <div class="row">
-                    <div class="col-md-8">
+            <img src="/static/site/images/core/manit.jpeg" alt="Bhopal">
+        </div>
 
-                        <!-- block  contact-->
-                        <div class="block-contact-us">
-                            <div class="block-title">
-                                contact us
+        <div class="row">
+            <div class="col-md-8">
+
+                <!-- block  contact-->
+                <div class="block-contact-us">
+                    <div class="block-title">
+                        contact us
+                    </div>
+
+                    <?php
+                            $errors=validation_errors("<div class='alert alert-danger'>",'</div>');
+   if($errors)
+                                echo $errors;
+                             elseif($message=="success")
+                                echo "<div class='alert alert-success'>".'Message sent Successfully.'."</div>";
+                            elseif($message=="sql_error")
+                                echo "<div class='alert alert-warning'>".'Some Error Occured.'."</div>";
+                            elseif($message="no_message")
+                                echo "<div class='alert alert-info'>".'Contact us by filling form.'."</div>";
+                          ?>
+                    <form action="/contact" method="post">
+                    <div class="block-content row">
+                        <div class="col-sm-5">
+                            <div class="form-group">
+                                <input type="text" placeholder="Name *" class="form-control" name="name">
                             </div>
-                            <div class="block-content row">
-                                <div class="col-sm-5">
-                                    <div class="form-group">
-                                        <input type="text" placeholder="Name *" class="form-control" id="name">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" placeholder="Email *" class="form-control" id="email">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" placeholder="Subject" id="subject" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-sm-7">
-                                    <div class="form-group">
-                                        <textarea placeholder="Message" rows="5" class="form-control" id="message"></textarea>
-                                    </div>
-                                    <div class="text-right">
-                                        <button class="btn btn-inline" type="submit">send message</button>
-                                    </div>
-                                </div>
-                            </div><!-- block  contact-->
-                            
+                            <div class="form-group">
+                                <input type="text" placeholder="Email *" class="form-control" name="email">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" placeholder="Mobile Number*" name="mobile" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" placeholder="Place*" name="place" class="form-control">
+                            </div>
+
                         </div>
-                    </div>
+                        <div class="col-sm-7">
 
-                    <div class="col-md-4">
-
-                         <!-- block  contact-->
-                        <div class="block-address">
-                            <div class="block-title">
-                                address
+                            <div class="form-group">
+                                <input type="text" placeholder="Subject*" name="subject" class="form-control">
                             </div>
-                            <div class="block-content ">
-                                <p>
-                                    <b class="title">Address</b>
-                                    Hostel 6 MANIT, Bhopal, Madhya Pradesh 462003
-                                </p>
-                                <p>
-                                    <b class="title">Phone number</b>
-                                    +91-9755984443
-                                </p>
-                                <p>
-                                    <b class="title">opening hours</b>
-                                    24 &times; 7
-                                </p>
+                            <div class="form-group">
+                                <textarea placeholder="Message" rows="5" class="form-control" name="message"></textarea>
                             </div>
-                            
-                        </div><!-- block  contact-->
-                    </div>
-                </div>
-
+                            <div class="text-right">
+                                <input class="btn btn-inline" type="submit">
+                            </div>
+                        </div>
+                    </div><!-- block  contact-->
+                    </form>
             </div>
-           
-		</main><!-- end MAIN -->
+
+            <div class="col-md-4">
+
+                <!-- block  contact-->
+                <div class="block-address">
+                    <div class="block-title">
+                        address
+                    </div>
+                    <div class="block-content ">
+                        <p>
+                            <b class="title">Address</b>
+                            Hostel 6 MANIT, Bhopal, Madhya Pradesh 462003
+                        </p>
+                        <p>
+                            <b class="title">Phone number</b>
+                            +91-9755984443
+                        </p>
+                        <p>
+                            <b class="title">opening hours</b>
+                            24 &times; 7
+                        </p>
+                    </div>
+
+                </div><!-- block  contact-->
+            </div>
+        </div>
+
+    </div>
+
+</main><!-- end MAIN -->
